f40469d1e88c236133767d8af05176565fbf3c9e
wisefool769 <aashiq.d@gmail.com>
2017-03-19 14:53:36 -0500

    bug fixes in Percolation

--- Percolation/Percolation.java
+++ Percolation/Percolation.java
@@ -1,102 +1,64 @@
+/*************************************************************************
+ *  Compilation:  javac-algs4 Percolation.java
+ *  Execution:    java-algs4  Percolation
+ *  Dependencies: WeightedQuickUnionUF
+ *
+ *  Implements a grid with cells that can be opened. Used for the first project
+ *  in Princeton Algs4 on Coursera. 
+ *
+ *************************************************************************/
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+    
 public class Percolation {
+    private int width;
     private int gridSize;
     private boolean[][] grid;
-    private int[] roots;
-    private int[] sizes;
+    private WeightedQuickUnionUF uf;
     private int nOpen;
+    // index of virtual top of grid in uf 
     private int topIdx;
+    // index of virtual bottom in uf
     private int bottomIdx;
     
     // create n-by-n grid, with all sites blocked
     public Percolation(int n) {
-        gridSize = n;
-        grid = new boolean[n][n];
-        roots = new int[n * n + 2];
-        sizes = new int[n * n + 2];
+        width = n;
+        gridSize = width + 1;
+        // n + 1 because indeces start at 1
+        grid = new boolean[gridSize][gridSize];
+        uf = new WeightedQuickUnionUF(width * width + width + 2);
         
-        topIdx = n * n;
+        topIdx = width * width + width;
         bottomIdx = topIdx + 1;
-        
-        roots[topIdx] = topIdx;
-        roots[bottomIdx] = bottomIdx;
-        
-        for (int i = n; i < n * (n - 1); i++) {
-            roots[i] = i;
-            sizes[i] = 1;
-        }
-        
-        for (int i = 0; i < n; i++) {
-            roots[i] = topIdx;
-            sizes[i] = n + 1;
-        }
-        
-        boolean alreadyConnected = false;
-        for (int i = n * n - n; i < n * n; i++) {
-            if (roots[i] != topIdx) {
-                roots[i] = bottomIdx;
-                sizes[i] = n + 1;
-            } 
-            else {
-                alreadyConnected = true;
-                break;
-            }
-         
-        }
 
-        if (alreadyConnected) {
-            roots[bottomIdx] = topIdx;
-        }
-        
-
-        for (int i = 0; i != n; i++) {
-            for (int j = 0; j != n; j++) {
+        for (int i = 1; i != gridSize; i++) {
+            for (int j = 0; j != gridSize; j++) {
                 grid[i][j] = false;
             }
         }
 
         nOpen = 0;
     }
-    
-    private int getRoot(int idx)
-    {
-        while (roots[idx] != idx) {
-            idx = roots[idx];
-        }
-
-        return idx;
+   
+    private int xyTo1D(int row, int col) {
+        return (row - 1) * width + (col - 1);
     }
     
-    private void union(int i, int j) {
-        int ri = getRoot(i);
-        int rj = getRoot(j);
-        if (ri != rj) {
-            if (sizes[ri] > sizes[rj]) {
-                roots[rj] = ri;
-                sizes[ri] += sizes[rj];
-            } 
-            else {
-                roots[ri] = rj;
-                sizes[rj] += sizes[ri];
-            }
-        }
+    private void validateIndices(int row, int col) {
+        if ((row < 1) || (row > width))
+            throw new IndexOutOfBoundsException("row index out of bounds");
+        if ((col < 1) || (col > width))
+            throw new IndexOutOfBoundsException("column index out of bounds");
     }
     
-    
-    private boolean connected(int i, int j) {
-        return getRoot(i) == getRoot(j);
-    }
-    
-    private int getRootIdx(int row, int col) {
-        return row * gridSize + col;
-    }
-    
-    private boolean inBounds(int axisVal) {
-        return ((axisVal > -1) && (axisVal < gridSize));
+    private boolean checkIndices(int row, int col){
+        return ((row > 0) && (row < gridSize) && (col > 0) && (col < gridSize));
     }
     
     private void connectCells(int r1, int c1, int r2, int c2) {
-        if (inBounds(r2) && inBounds(c2) && isOpen(r2, c2)) {
-            union(getRootIdx(r1, c1), getRootIdx(r2, c2));
+        if (checkIndices(r2, c2) && isOpen(r2, c2)) {
+            uf.union(xyTo1D(r1, c1), xyTo1D(r2, c2));
         }
     }
     
@@ -105,11 +67,18 @@ public class Percolation {
         connectCells(r1, c1, r1, c1 + 1);
         connectCells(r1, c1, r1 - 1, c1);
         connectCells(r1, c1, r1, c1 - 1);
+        if(r1 == 1){
+            uf.union(xyTo1D(r1, c1), topIdx);
+        }
+        if(r1 == width){
+            uf.union(xyTo1D(r1, c1), bottomIdx);
+        }
     }
     
     // open site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (isFull(row, col)) {
+        validateIndices(row, col);
+        if (!grid[row][col]) {
             grid[row][col] = true;
             nOpen++;
             connectNeighbors(row, col);
@@ -118,11 +87,13 @@ public class Percolation {
     
     // is site (row, col) open?
     public boolean isOpen(int row, int col) {
+        validateIndices(row, col);
         return grid[row][col];
     }
    // is site (row, col) full?
     public boolean isFull(int row, int col)  {
-        return (!grid[row][col]);
+        validateIndices(row, col);
+        return uf.connected(xyTo1D(row, col), topIdx);
     }
     
    // number of open sites
@@ -132,7 +103,7 @@ public class Percolation {
     
     // does the system percolate?
     public boolean percolates() {
-        return connected(topIdx, bottomIdx);
+        return uf.connected(topIdx, bottomIdx);
     }
   // test client (optional)
     public static void main(String[] args) {
